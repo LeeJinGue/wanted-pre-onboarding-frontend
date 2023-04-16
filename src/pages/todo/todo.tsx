dc8203b1562e81aa044785ccfbc7b9c0d3ea7431
LeeJinGue <hippo9851@gmail.com>
2023-04-17 04:58:10 +0900

    feat: todo 페이지 컴포넌트 구현
    - 헤더만 구현

---
+++ src/pages/todo/todo.tsx
@@ -0,0 +1,12 @@
+import { Container, Flex } from "@chakra-ui/react"
+
+const Todo = () => {
+  return (<>
+  <Container size="sm">
+    <Flex flexDir="column">
+      Todo 페이지입니다.
+    </Flex>
+  </Container>
+  </>)
+}
+export default Todo
