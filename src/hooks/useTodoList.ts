72b96a7588d9cad31757b895c28052ea787b3fb3
LeeJinGue <hippo9851@gmail.com>
2023-04-19 15:11:09 +0900

    feat: useTodoList 훅 구현
    - Todo List를 위한 커스텀 훅 구현
    - Todo 업데이트, 삭제, 추가 함수 구현

---
+++ src/hooks/useTodoList.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { deleteTodo } from "../api/axios/delete"
+import { createTodos } from "../api/axios/post"
+import { updateTodos } from "../api/axios/put"
+
+const useTodoList = (initData: TodoDTOT[]) => {
+  const [todoList, setTodoList] = useState<TodoDTOT[]>(() => initData)
+  const updateWithTodo = async (updateTodo: updateTodosProps) => {
+    const updateRes = await updateTodos(updateTodo)
+    setTodoList(todoList.map((todoData) => {
+      if (todoData.id === updateRes.id) return updateRes
+      else return todoData
+    }))
+    return updateRes
+  }
+  const deleteWithId = async (deleteId: number) => {
+    const isDelete = await deleteTodo({ id: deleteId })
+    if(isDelete) setTodoList(todoList.filter((todoData) => todoData.id !== deleteId))
+    return isDelete
+  }
+  const addWithText = async (addTodoText: string) => {
+    const createRes = await createTodos({ todo: addTodoText });
+    if (createRes) {
+      setTodoList(todoList.concat(createRes));
+    }
+    return createRes
+  }
+  return {todoList, updateWithTodo, deleteWithId, addWithText}
+}
+
+export default useTodoList
