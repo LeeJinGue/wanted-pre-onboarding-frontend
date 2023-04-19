c0465f690a1f8344e0d1497fbe84c7d661de8b9b
LeeJinGue <hippo9851@gmail.com>
2023-04-19 15:20:19 +0900

    refactor: 필요없는 파일 및 import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Signin from "./pages/signin"
 import Signup from "./pages/signup"
