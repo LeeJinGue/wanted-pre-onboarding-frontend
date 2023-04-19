7c5def1b78f2026ff21aae0a1d312d667af95c0e
LeeJinGue <hippo9851@gmail.com>
2023-04-19 14:59:59 +0900

    fix: get API 수정
    - 조회 실패시 실패 데이터 return 하도록 구현

--- src/api/axios/get.ts
+++ src/api/axios/get.ts
@@ -9,6 +9,7 @@ export const getTodos = async () => {
     // 조회 성공시 성공한 데이터를 return합니다.
     return getRes.data
   }else{
-    console.log("# getTodos 에러:",getRes)
+    // 조회 실패시 실패 데이터 return
+    return getRes.data
   }
 }
